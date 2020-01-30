4ea23410df0c8c47c9beb004a2afdf219aa2629f
Armando Meza Leyva <armezaleyva@gmail.com>
2020-01-30 07:25:07 -0700

    Initial commit

---
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -0,0 +1,79 @@
+// PlantillaGraficos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GLEW_STATIC
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#include <iostream>
+
+using namespace std;
+
+void dibujar() {
+	glPushMatrix();
+
+	glEnd();
+	glPopMatrix();
+}
+
+int main() {
+	// Declarar una ventana
+	GLFWwindow* window;
+
+	// Si no se pudo iniciar GLFW
+	// Terminamos ejecución
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+
+	// Si se pudo iniciar GLFW
+	// Inicializamos la ventana
+	//window = glfwCreateWindow(1500, 600, "Ventana", NULL, NULL);
+	window = glfwCreateWindow(600, 600, "Ventana", NULL, NULL);
+
+	// Si no se pudo crear la ventana
+	// Terminamos la ejecución
+	if (!window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	// Establecemos la ventana como contexto
+	glfwMakeContextCurrent(window);
+
+	// Una vez establecido el contexto se activan las funciones "modernas" (gpu)
+	glewExperimental = true;
+
+	GLenum errores = glewInit();
+	if (errores != GLEW_OK) {
+		glewGetErrorString(errores);
+	}
+
+	const GLubyte *versionGL = glGetString(GL_VERSION);
+	cout << "Version OpenGL: " << versionGL;
+
+	// Ciclo de dibujo (Draw loop)
+	while (!glfwWindowShouldClose(window)) {
+		// Establecer región de dibujo
+		//glViewport(0, 0, 1500, 600);
+		glViewport(0, 0, 600, 600);
+		// Establecemos el color de borrado
+		glClearColor(1, 0.7, 0.5, 1);
+		// Borrar!
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Actualizar valores y dibujar
+		dibujar();
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+	// Después del ciclo de dibujo
+	// Eliminamos ventana y procesos de glfwr
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
